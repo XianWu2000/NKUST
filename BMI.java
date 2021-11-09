259d7dbf2779b2d3e8b505f6090820db9a784ca0
XianWu2000 <93572203+XianWu2000@users.noreply.github.com>
2021-11-09 16:31:16 +0800

    Add files via upload

---
+++ BMI.java
@@ -0,0 +1,3 @@
+public class BMI {
+    public static void main(String[] args) {
+}
---
+++ variable.java
@@ -0,0 +1,8 @@
+public class variable {
+    public static void main(String[] args) {
+        // This is a comment
+        //lowerCamelCase variable name
+        String myName = "Xian";
+        System.out.println("Hello " + myName);
+    }
+}
