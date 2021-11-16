164919ca6793e62bfe01bc8be0b7c7fa46b8d18f
XianWu2000 <yisenww@gmail.com>
2021-11-16 15:57:20 +0800

    update

--- BMI.java
+++ BMI.java
@@ -1,3 +1,8 @@
 public class BMI {
     public static void main(String[] args) {
+        float h = 1.70f;
+        int w = 61;
+        float BMI = w / (h * h);
+        System.out.println(BMI);
+    }
 }
---
+++ EFU.java
@@ -0,0 +1,5 @@
+public class EFU {
+    public static void main(String[] args){
+
+    }
+}
